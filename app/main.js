6413424fedb72e4142cb5920bbe0f86d04ca6d85
Haider Alshamma <haidera@nulogy.com>
2023-10-09 17:39:53 -0400

    Pass stage 3

--- app/main.js
+++ app/main.js
@@ -7,27 +7,48 @@ const CRLF = "\r\n"
 
 const withCRLF = (s) => s + CRLF
 
+const parsePathURI = (path, base) => {
+    const parts = path.split(`/${base}/`)
+
+    return parts[parts.length - 1]
+}
+
+const parseBase = (path) => {
+  return path.split("/")[1]
+}
+
 const parseRequestData = (data) => {
   let [startLine] = data.toString().split(CRLF)
-  let path =  startLine.split(" ")[1]
 
-  return { path }
+  let path =  startLine.split(" ")[1]
+  let base = parseBase(path)
+  const uri = parsePathURI(path, base)
+
+  return { path, uri }
 }
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const { path } = parseRequestData(data)
+    const { path, uri } = parseRequestData(data)
 
-    let HttpStatusLine = ""
+    let httpStatusLine = withCRLF("")
+    let contentType = withCRLF("")
+    let contentLength = withCRLF("")
+    let responseBody = withCRLF("")
     let responseHeaders = withCRLF("")
 
     if (path === "/") {
-      HttpStatusLine = withCRLF("HTTP/1.1 200")
+      httpStatusLine = withCRLF("HTTP/1.1 200")
+    } else if (path.startsWith("/echo")) {
+      httpStatusLine = withCRLF("HTTP/1.1 200")
+      contentType = withCRLF("Content-Type: text/plain")
+      contentLength = withCRLF(`Content-Length: ${uri.length}`)
+      responseBody = withCRLF(uri)
     } else {
-      HttpStatusLine = withCRLF("HTTP/1.1 404")
+      httpStatusLine = withCRLF("HTTP/1.1 404")
     }
 
-    socket.write(HttpStatusLine + responseHeaders)
+    socket.write(httpStatusLine + contentType + contentLength  + responseHeaders + responseBody)
     socket.end()
   });
 
